2662dad71d229338ff9efb986e0e431ef5b80521
Pin Lin <moneycat711@gmail.com>
2020-05-11 17:15:47 +0800

    Add migration AddCollection

---
+++ HolyLibraryBackend/Migrations/20200511091538_AddCollection.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HolyLibraryBackend.Migrations
+{
+    public partial class AddCollection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Collections",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Isbn = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    Publisher = table.Column<string>(nullable: true),
+                    Price = table.Column<int>(nullable: false),
+                    Location = table.Column<string>(nullable: true),
+                    BorrowerId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Collections", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Collections_Users_BorrowerId",
+                        column: x => x.BorrowerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Collections_BorrowerId",
+                table: "Collections",
+                column: "BorrowerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Collections");
+        }
+    }
+}
