e5b7d6bcc5c544251af59759c8a0a2852dacba25
Pin Lin <moneycat711@gmail.com>
2020-05-11 17:39:48 +0800

    Add migration AddReaderAndManager

---
+++ HolyLibraryBackend/Migrations/20200511093245_AddReaderAndManager.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HolyLibraryBackend.Migrations
+{
+    public partial class AddReaderAndManager : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Users",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Users");
+        }
+    }
+}
