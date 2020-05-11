5fa0b2c9a908107138745f9571a0433f417f45e4
Pin Lin <moneycat711@gmail.com>
2020-05-11 14:58:12 +0800

    Demo creating User

---
+++ HolyLibraryBackend/Dto/CreateUserDto.cs
@@ -0,0 +1,10 @@
+namespace HolyLibraryBackend.Dto
+{
+    public class CreateUserDto
+    {
+        public string Account { get; set; }
+        public string Pasword { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
