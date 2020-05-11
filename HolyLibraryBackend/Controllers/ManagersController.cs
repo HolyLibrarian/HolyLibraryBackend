2b56f6e3fad5a88248ec867f9df2b32729836722
Pin Lin <moneycat711@gmail.com>
2020-05-12 02:06:31 +0800

    Replace UsersController with ReadersController and ManagerController

---
+++ HolyLibraryBackend/Controllers/ManagersController.cs
@@ -0,0 +1,32 @@
+﻿using HolyLibraryBackend.Dto;
+using HolyLibraryBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolyLibraryBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ManagersController : ControllerBase
+    {
+        private readonly HolyLibraryContext dbContext;
+
+        public ManagersController(HolyLibraryContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public Manager CreateManager(CreateManagerDto createManagerDto)
+        {
+            var manager = new Manager
+            {
+                Account = createManagerDto.Account,
+                Password = createManagerDto.Password,
+                Name = createManagerDto.Name,
+            };
+            dbContext.Add(manager);
+            dbContext.SaveChanges();
+            return manager;
+        }
+    }
+}
---
+++ HolyLibraryBackend/Controllers/ReadersController.cs
@@ -0,0 +1,35 @@
+﻿using HolyLibraryBackend.Dto;
+using HolyLibraryBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolyLibraryBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReadersController : ControllerBase
+    {
+        private readonly HolyLibraryContext dbContext;
+
+        public ReadersController(HolyLibraryContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public Reader CreateReader(CreateReaderDto createReaderDto)
+        {
+            var reader = new Reader
+            {
+                Account = createReaderDto.Account,
+                Password = createReaderDto.Password,
+                Name = createReaderDto.Name,
+                Email = createReaderDto.Email,
+                PhoneNumber = createReaderDto.PhoneNumber,
+                MaxBorrowNumber = createReaderDto.MaxBorrowNumber,
+            };
+            dbContext.Add(reader);
+            dbContext.SaveChanges();
+            return reader;
+        }
+    }
+}
