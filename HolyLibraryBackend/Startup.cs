fab8a3b4fd52b7be330587c634522ac994fd49c5
Pin Lin <moneycat711@gmail.com>
2020-05-12 14:43:23 +0800

    Enable CORS

--- HolyLibraryBackend/Startup.cs
+++ HolyLibraryBackend/Startup.cs
@@ -23,6 +23,15 @@ namespace HolyLibraryBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                });
+            });
             services.AddControllers();
             services.AddDbContext<HolyLibraryContext>(options =>
                 options.UseMySql(Configuration.GetConnectionString("Database")));
@@ -57,6 +66,8 @@ namespace HolyLibraryBackend
 
             app.UseAuthentication();
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
