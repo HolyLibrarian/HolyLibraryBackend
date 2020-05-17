202b01e5d3b674970a6c47505e83516d2bf82ad4
Pin Lin <moneycat711@gmail.com>
2020-05-18 02:35:57 +0800

    TestCreateReader

---
+++ HolyLibraryBackend.Test/Controllers/ReadersControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using HolyLibraryBackend.Controllers;
+using HolyLibraryBackend.Dto;
+using HolyLibraryBackend.Models;
+
+namespace HolyLibraryBackend.Test
+{
+    public class ReadersControllerTest
+    {
+        private ReadersController readersController;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<HolyLibraryContext>()
+                .UseInMemoryDatabase("HolyLibrary")
+                .Options;
+            var dbContext = new HolyLibraryContext(options);
+            readersController = new ReadersController(dbContext);
+        }
+
+        [Test]
+        public void TestCreateReader()
+        {
+            var createReaderDto = new CreateReaderDto
+            {
+                Password = "",
+            };
+            var result = readersController.CreateReader(createReaderDto);
+            Assert.IsNotNull(result);
+        }
+    }
+}
