66b2c3eb0ed221882cb2e5b27e382ba286122743
Pin Lin <moneycat711@gmail.com>
2020-05-18 04:34:56 +0800

    ManagersControllerTest

---
+++ HolyLibraryBackend.Test/Controllers/ManagersControllerTest.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using HolyLibraryBackend.Controllers;
+using HolyLibraryBackend.Dto;
+using HolyLibraryBackend.Models;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace HolyLibraryBackend.Test
+{
+    public class ManagersControllerTest
+    {
+        private ManagersController managersController;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<HolyLibraryContext>()
+                .UseInMemoryDatabase("HolyLibrary")
+                .Options;
+            var dbContext = new HolyLibraryContext(options);
+            managersController = new ManagersController(dbContext);
+        }
+
+        [Test]
+        public void TestCreateReader()
+        {
+            var result = managersController.CreateManager(new CreateManagerDto
+            {
+                Password = "",
+            });
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void TestSearchReaders()
+        {
+            managersController.CreateManager(new CreateManagerDto
+            {
+                Account = "123456",
+                Password = "",
+                Name = "123456",
+                Email = "123456@ntut.edu.tw",
+            });
+            managersController.CreateManager(new CreateManagerDto
+            {
+                Account = "456789",
+                Password = "",
+                Name = "456789",
+                Email = "456789@ntut.edu.tw",
+            });
+
+            var result = managersController.SearchManagers(account: "456");
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void TestGetReader()
+        {
+            managersController.CreateManager(new CreateManagerDto
+            {
+                Password = "",
+            });
+            var result = managersController.GetManager(1);
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void TestEditReader()
+        {
+            managersController.CreateManager(new CreateManagerDto
+            {
+                Password = "",
+            });
+            var result = managersController.EditManager(1, new EditManagerDto
+            {
+                Account = "test",
+                Password = "",
+            });
+            Assert.IsNotNull(result);
+        }
+    }
+}
--- HolyLibraryBackend/Controllers/ManagersController.cs
+++ HolyLibraryBackend/Controllers/ManagersController.cs
@@ -2,6 +2,8 @@
 using HolyLibraryBackend.Models;
 using Isopoh.Cryptography.Argon2;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace HolyLibraryBackend.Controllers
 {
@@ -30,6 +32,47 @@ namespace HolyLibraryBackend.Controllers
             dbContext.Add(manager);
             dbContext.SaveChanges();
             return Created(manager.Id.ToString(), manager);
+        }
+
+        [HttpGet]
+        public IActionResult SearchManagers(string account = null, string name = null, string email = null, string phoneNumber = null)
+        {
+            var managers = dbContext.Managers
+                .Where(x => x.Account.Contains(account) || account == null)
+                .Where(x => x.Name.Contains(name) || name == null)
+                .Where(x => x.Email.Contains(email) || email == null)
+                .Where(x => x.PhoneNumber.Contains(phoneNumber) || phoneNumber == null)
+                .ToList();
+            return Ok(managers);
+        }
+
+        [HttpGet("{userId}")]
+        public IActionResult GetManager(int userId)
+        {
+            var manager = dbContext.Managers.Where(x => x.Id == userId).FirstOrDefault();
+            if (manager == null)
+            {
+                return NotFound();
+            }
+            return Ok(manager);
+        }
+
+        [HttpGet("{userId}")]
+        public IActionResult EditManager(int userId, EditManagerDto editManagerDto)
+        {
+            var manager = dbContext.Managers.Where(x => x.Id == userId).FirstOrDefault();
+            if (manager == null)
+            {
+                return NotFound();
+            }
+            manager.Account = editManagerDto.Account;
+            manager.Password = Argon2.Hash(editManagerDto.Password);
+            manager.Name = editManagerDto.Name;
+            manager.Email = editManagerDto.Email;
+            manager.PhoneNumber = editManagerDto.PhoneNumber;
+            dbContext.Update(manager);
+            dbContext.SaveChanges();
+            return Ok(manager);
         }
     }
 }
