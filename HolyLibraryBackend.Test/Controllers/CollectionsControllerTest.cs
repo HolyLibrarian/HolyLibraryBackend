13da7c513558436801da2da8fc4acae5326fff85
Pin Lin <moneycat711@gmail.com>
2020-05-18 07:11:00 +0800

    TestCreateCollection

---
+++ HolyLibraryBackend.Test/Controllers/CollectionsControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using HolyLibraryBackend.Controllers;
+using HolyLibraryBackend.Dto;
+using HolyLibraryBackend.Models;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace HolyLibraryBackend.Test
+{
+    public class CollectionsControllerTest
+    {
+        private CollectionsController collectionsController;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<HolyLibraryContext>()
+                .UseInMemoryDatabase("HolyLibrary")
+                .Options;
+            var dbContext = new HolyLibraryContext(options);
+            collectionsController = new CollectionsController(dbContext);
+        }
+
+        [Test]
+        public void TestCreateCollection()
+        {
+            var result = collectionsController.CreateCollection(new CreateCollecitonDto());
+            Assert.IsNotNull(result);
+        }
+    }
+}
