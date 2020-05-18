73be4db474252e5de4319eb3c0afadf984e1094b
Pin Lin <moneycat711@gmail.com>
2020-05-18 20:57:59 +0800

    Add migration AddFlagsToBorrowRecordAndReservation

---
+++ HolyLibraryBackend/Migrations/20200518125711_AddFlagsToBorrowRecordAndReservation.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HolyLibraryBackend.Migrations
+{
+    public partial class AddFlagsToBorrowRecordAndReservation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCanceled",
+                table: "Reservations",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFulfilled",
+                table: "Reservations",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReturned",
+                table: "BorrowRecords",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCanceled",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "IsFulfilled",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "IsReturned",
+                table: "BorrowRecords");
+        }
+    }
+}
